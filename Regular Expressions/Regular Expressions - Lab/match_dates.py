fe7b37878dc7c403b04940fa84fa65643e010056
Gabriela-V-I <gabivanova96@gmail.com>
2022-12-08 20:05:58 +0100

    Add task for Regular Expressions - Lab

---
+++ Regular Expressions/Regular Expressions - Lab/match_dates.py
@@ -0,0 +1,7 @@
+import re
+
+dates = input()
+pattern = r"(\d{2})([/.-])([A-z][a-z]{2})\2(\d{4})"
+result = re.findall(pattern, dates)
+for match in result:
+    print(f"Day: {match[0]}, Month: {match[2]}, Year: {match[3]}")
